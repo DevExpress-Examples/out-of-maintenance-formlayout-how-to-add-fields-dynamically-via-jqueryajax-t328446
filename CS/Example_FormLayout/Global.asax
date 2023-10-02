bc5e913e3dc2ec434be6e6acad57f6aa21e0d98b
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-10-02 15:57:34 +0400

    Change example status to out of maintenance

--- CS/Example_FormLayout/Global.asax
+++
@@ -1,1 +0,0 @@
-<%@ Application Codebehind="Global.asax.cs" Inherits="Example_FormLayout.MvcApplication" Language="C#" %>
--- VB/Example_FormLayout_VB/Models/Category.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Category
-    Public Property ID As Integer
-    Public Property Name As String
-    Public Property Products As List(Of Product)
-End Class
-
-Public Class Product
-    Public Property ProductName As String
-End Class
